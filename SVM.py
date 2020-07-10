47b2e61462f700a5f94658d5be95ce874d54d060
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-10 03:49:06 +0100

    Dataset reading into the project and plotting based on clump and uniformsize

--- SVM.py
+++ SVM.py
@@ -8,3 +8,33 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 # %matplotlib inline  #useful in Jupyter notebooks
 
+
+#============================================LOADING THE DATA================================
+"""
+    ID	            Clump thickness
+    Clump	        Clump thickness
+    UnifSize	    Uniformity of cell size
+    UnifShape	    Uniformity of cell shape
+    MargAdh	        Marginal adhesion
+    SingEpiSize	    Single epithelial cell size
+    BareNuc	        Bare nuclei
+    BlandChrom	    Bland chromatin
+    NormNucl	    Normal nucleoli
+    Mit	            Mitoses
+    Class	        Benign or malignant
+"""
+cell_df = pd.read_csv("cell_samples.csv")
+dataset = cell_df.head()
+print(dataset)
+
+# The values are graded from 1 to 10, with 1 being the closest to benign.
+# The Class field contains the diagnosis, as confirmed by separate medical procedures, as to whether the samples are benign (value = 2) or malignant (value = 4).
+
+#========================distribution based on clump thickness and uniformity of cell size.
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant');
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax);
+plt.show()
+
+
+
+
