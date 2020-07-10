9ad44f2fb87b84558ad844e8d5b9a7f9461f39eb
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-10 03:40:56 +0100

    Initial project set-up with requirements imported

---
+++ SVM.py
@@ -0,0 +1,10 @@
+#================================IMPORTING REQUIRED MODULES, PACKAGES AND LIBRARIES=========================
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+# %matplotlib inline  #useful in Jupyter notebooks
+
